77c2aac011c976a0ffbe5af61adb61bb487ceb99
coderbaozi <YHbaozi1988@163.com>
2023-05-06 21:53:16 +0800

    feat: add colorPicker

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,24 +1,40 @@
-import { Component, createEffect, onMount } from "solid-js";
+import { Component, Setter, createEffect, onMount } from "solid-js";
 import { Accessor } from "solid-js";
 interface IProps {
   name: Accessor<string>;
+  color: Accessor<string | undefined>;
+  setCanvas: Setter<HTMLCanvasElement>;
 }
-const Avatar: Component<IProps> = ({ name }) => {
+const Avatar: Component<IProps> = ({ name, color, setCanvas }) => {
+  let dpr = window.devicePixelRatio;
+  let first = true;
   let canvas: HTMLCanvasElement = {} as HTMLCanvasElement;
   createEffect(() => {
+    setCanvas(canvas);
     const ctx = canvas.getContext("2d");
+    if (first) {
+      fixDistortion();
+      first = !first;
+    }
     if (ctx) {
-      ctx.fillStyle = "#fff";
+      ctx.fillStyle = (color() === undefined ? "#fff" : color()) as string;
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = "#000";
       ctx.font = "36px Helvetica Neue";
       ctx.textAlign = "center";
       ctx.textBaseline = "middle";
       // substring is [ )
-      ctx.fillText(name().substring(0, 2), canvas.width / 2, canvas.height / 2);
+      ctx.fillText(name().substring(0, 2), canvas.width / 3, canvas.height / 3);
     }
   });
-
+  function fixDistortion() {
+    let { width: cssWidth, height: cssHeight } = canvas.getBoundingClientRect();
+    canvas.style.width = canvas.width + "px";
+    canvas.style.height = canvas.height + "px";
+    canvas.width = dpr * cssWidth;
+    canvas.height = dpr * cssHeight;
+    canvas.getContext("2d")?.scale(dpr, dpr);
+  }
   return (
     <div class="mb-4 p-4">
       <canvas
--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -1,27 +1,83 @@
-import type { Component } from "solid-js";
-import { createSignal } from "solid-js";
+import { Component, onMount } from "solid-js";
+import { createSignal, Show } from "solid-js";
 import Avatar from "./Avatar";
 import Input from "./Input";
 import { Icon } from "@iconify-icon/solid";
 
 const Center: Component = () => {
-  const [name, setName] = createSignal<string>("包子");
+  const [name, setName] = createSignal<string>("♻️");
+  const [show, setShow] = createSignal<boolean>(false);
+  const [color, setColor] = createSignal<string>();
+  const [canvas, setCanvas] = createSignal<HTMLCanvasElement>(
+    {} as HTMLCanvasElement
+  );
+  let picker: HTMLInputElement = {} as HTMLInputElement;
+
+  const autoOpenPicker = () => {
+    picker.click();
+  };
+
+  const handleRecovery = () => {
+    window.location.reload();
+  };
+
+  const downloadCanvas = (blob: Blob, fileName: string) => {
+    const a = document.createElement("a");
+    a.style.display = "none";
+    const url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+  };
+
   return (
     <>
       <div class="flex text-xl flex-col items-center shadow-md justify-center bg-light-200  p-4">
-        <Avatar name={name} />
+        <Avatar setCanvas={setCanvas} color={color} name={name} />
         <div class="flex space-x-4">
-          <div class="cursor-pointer hover:bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-100 flex p-4 rounded-full bg-slate-100  text-black">
+          <div
+            onClick={handleRecovery}
+            class="cursor-pointer hover:bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-100 flex p-4 rounded-full bg-slate-100  text-black"
+          >
             <Icon icon="ic:round-refresh" />
           </div>
-          <button class="flex items-center transition hover:bg-gradient-to-r from-green-400 to-blue-200 inline-block rounded-3xl bg-slate-100 px-8 py-3 text-sm font-medium text-black">
+          <button
+            onClick={() => {
+              canvas().toBlob((blob) => {
+                if (blob !== null) {
+                  downloadCanvas(blob, "avatar");
+                }
+              });
+            }}
+            class="flex items-center transition hover:bg-gradient-to-r from-green-400 to-blue-200 inline-block rounded-3xl bg-slate-100 px-8 py-3 text-sm font-medium text-black"
+          >
             <Icon class="text-xl" icon="material-symbols:download" />
             Export PNG
           </button>
           <button class="flex items-center hover:bg-gradient-to-r from-green-400 to-blue-50 inline-block rounded-3xl bg-slate-100 px-8 py-3 text-sm font-medium text-black">
             <Icon class="text-xl" icon="material-symbols:download" />
-            Export SVG
+            Export SVG(Wait)
           </button>
+          <div
+            onClick={() => {
+              setShow((show) => !show);
+              autoOpenPicker();
+            }}
+            class="relative cursor-pointer hover:bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-100 flex p-4 rounded-full bg-slate-100  text-blac"
+          >
+            <Icon icon="ic:outline-color-lens" />
+            <Show when={show()} fallback={() => <></>}>
+              <input
+                onChange={() => {
+                  setColor(picker.value);
+                }}
+                ref={picker}
+                class="hidden absolute -top-8 -right-14"
+                type="color"
+                value="#fffaaa"
+              />
+            </Show>
+          </div>
         </div>
         <Input handleChange={setName} />
       </div>
